644f1d7437a8314361ee896dbf73a7e42de1e74d
Marcelo Truque <machotm@hotmail.com>
2021-08-22 04:20:46 -0600

    Clase Page con características para el algoritmo de reemplazo y que almacena 1kb de enteros

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
 class QuickSort{
+public:
+    //swap cambia las variables en caso de ser menor que el pivot
     void swap(int*a,int*b){
         int t = *a;
         *a=*b;
         *b=t;
     }
+    // se encarga de hacer la partición y el ordenamiento en base al pivot
+    // (se decidió que el pivot fuera el último elemento del arreglo)
     int partition(int arr[],int low, int high){
         int pivot=arr[high];
         int i =(low-1);
-        for (int j=low;j <high-1;j++){
+        for (int j=low;j <=high-1;j++){
             if(arr[j]<pivot){
                 i++;
                 swap(&arr[i],&arr[j]);
@@ -17,6 +25,7 @@ class QuickSort{
         swap(&arr[i+1],&arr[high]);
         return(i+1);
     }
+    //Maneja el ordenamiento
     void quicksort(int arr[],int low, int high){
         if(low<high){
             int pi = partition(arr,low,high);
@@ -25,12 +34,40 @@ class QuickSort{
             quicksort(arr,pi+1,high);
         }
     }
-    QuickSort(int arr[], int low, int high ){
-
+    static void printarray(int arr[], int size){
+        int i;
+        for (i=0;i<size;i++){
+            cout <<arr[i]<< " ";
+            cout<< std::endl;
+        }
+    }
+    QuickSort(int arr[],int low,int high){
+        quicksort(arr,low,high);
         }
 
 };
+class Page{
+public:
+    int arr[256];
+    int use=1000000;
+    int number;
+    //Cada página registra si fue usada o no
+    void used(int usada){
+        use=use/10;
+        if (usada==1){
+
+            use+=10000000;
+        }
+    }
+
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int arr[6]={5,2,4,7,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    QuickSort(arr,0,n-1);
+    QuickSort::printarray(arr,n);
+
     return 0;
 }
