b5e3c3e0b5d659bd251e6cef21060cd2356cc1e7
Marcelo Truque <machotm@hotmail.com>
2021-08-23 21:54:49 -0600

    funciones de manejo de memoria y datos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -48,26 +49,83 @@ public:
 };
 class Page{
 public:
-    int arr[256];
-    int use=1000000;
-    int number;
+    int arr[256];//almacenamiento de 1kb de enteros
+    int index;
+    int use=1000000;//registro de uso de cada página
     //Cada página registra si fue usada o no
-    void used(int usada){
-        use=use/10;
-        if (usada==1){
 
+    void used(bool usada){
+        use= round(use/10);
+        if (usada==1){
             use+=10000000;
         }
     }
+};
 
 
+class MemoryManager{
+public:
+  Page Pages[6];
+  int size=0;
+  void writePage(Page page){
+      int begg=256*(page.index-1);
+      int final=256*(page.index);
+  }
+  int* readPage(int number){
+
+
+  }
+
+  int Pagedarray(int index){
+      float dif=index/256;//encontrar la página necesaria
+      int page = round(dif);//redondeo ya que la pagina es un entero
+
+      for(int i=0;i<6;i++){//Revisar memoria por la página deseada
+          Page checkPage = Pages[i];
+
+          if(checkPage.index==page){ //La pagina está en memoria
+              return checkPage.arr[index-(256*page)];//devolver el índice correspondiente
+          }
+          int PageFault(page);//La página no está en memoria
+
+      }
+
+  }
+  int PageFault(int page){
+      int exchange=searchexchange();// busca la página menos usada
+
+
+
+  }
+  int searchexchange(){
+      int leastused= Pages[0].use;
+      int i;
+      for( i=1;i<6;i++){
+          if(Pages[i].use<leastused){
+              leastused=Pages[i].use;
+              continue;
+          }
+      }
+      return i;
+  }
+
+  void filesize(){
+      fstream OldFile;
+      OldFile.open("C:\\Users\\Lenovo\\CLionProjects\\untitled1\\PruebaT1.txt",ios::in);
+      char number;
+      char lim[2] ="," ;
+      while (OldFile.get(number)){
+          if (number==*lim){
+              size+=1;
+          }
+      }
+      OldFile.close();
+  }
 };
 
 int main() {
     int arr[6]={5,2,4,7,9};
     int n = sizeof(arr)/sizeof(arr[0]);
-    QuickSort(arr,0,n-1);
-    QuickSort::printarray(arr,n);
-
+    MemoryManager MMU;
     return 0;
 }
